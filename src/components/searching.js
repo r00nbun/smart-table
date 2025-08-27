7abc263974738cb24c4a7dd85043ef7e73c476fe
r00nbun <r00nbun@gmail.com>
2025-08-27 03:54:17 +0300

    Выполненный проект: спринт 7

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,21 +1,7 @@
-import {rules, createComparison} from "../lib/compare.js";
-
-
 export function initSearching(searchField) {
-    // @todo: #5.1 — настроить компаратор
-    const compare = createComparison(
-        ['skipNonExistentSourceFields',
-        'skipEmptyTargetValues',
-        'failOnEmptySource',
-        'arrayAsRange',
-        'stringIncludes',
-        'exactEquality'],
-        [rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)]
-    );
-
-
-    return (data, state, action) => {
-        // @todo: #5.2 — применить компаратор
-        return data.filter(row => compare(row, state));
-    }
+    return (query, state, action) => {
+        return state[searchField] ? Object.assign({}, query, {
+            search: state[searchField]
+        }) : query;
+    } 
 }
--- src/main.js
+++ src/main.js
@@ -7,15 +7,12 @@ import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
-// @todo: подключение
 import {initPagination} from './components/pagination.js';
 import {initSorting} from './components/sorting.js';
 import {initFiltering} from './components/filtering.js';
 import {initSearching} from './components/searching.js';
 
-
-// Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData(sourceData);
 
 /**
  * Сбор и обработка полей из таблицы
@@ -37,17 +34,20 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
-    let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
-    result = applySearching(result, state, action);
-    result = applyFiltering(result, state, action);
-    result = applySorting(result, state, action);
-    result = applyPagination(result, state, action);
+async function render(action) {
+    let state = collectState();
+    let query = {};
 
+    query = applySearching(query, state, action);
+    query = applyFiltering(query, state, action);
+    query = applySorting(query, state, action);
+    query = applyPagination(query, state, action);
 
-    sampleTable.render(result)
+    const { total, items } = await api.getRecords(query);
+
+    updatePagination(total, query);
+    sampleTable.render(items);
+
 }
 
 const sampleTable = initTable({
@@ -57,8 +57,7 @@ const sampleTable = initTable({
     after: ['pagination']
 }, render);
 
-// @todo: инициализация
-const applyPagination = initPagination(
+const {applyPagination, updatePagination} = initPagination(
     sampleTable.pagination.elements,
     (el, page, isCurrent) => {
         const input = el.querySelector('input');
@@ -75,14 +74,19 @@ const applySorting = initSorting([
     sampleTable.header.elements.sortByTotal
 ]); 
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {
-    searchBySeller: indexes.sellers
-});
+const {applyFiltering, updateIndexes} = initFiltering(sampleTable.filter.elements);
 
 const applySearching = initSearching(sampleTable.search.elements, 'search');
-
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
-render();
+async function init() {
+    const indexes = await api.getIndexes();
+
+    updateIndexes(sampleTable.filter.elements, {
+        searchBySeller: indexes.sellers
+    });
+}
+
+init().then(render);
